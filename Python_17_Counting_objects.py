81a74d8e643934603734b7e1bf3c56c0700e3805
Yuxiao Zhang <ttcmk@yahoo.com>
2017-02-27 13:55:07 -0500

    Create Python_17_Counting_objects.py

---
+++ Python_17_Counting_objects.py
@@ -0,0 +1,23 @@
+"""
+输入一行字符，分别统计出其中英文字母、空格、数字和其它字符的个数。
+"""
+
+def Counting(s):
+    l = list(s)
+    number = 0
+    letter = 0
+    space = 0
+    other = 0
+    for i in l:
+        if i.isalpha():
+            letter += 1
+        elif i.isspace():
+            space += 1
+        elif i.isdigit():
+            number += 1
+        else:
+            other += 1
+    print("number ---> %d, space ---> %d, letter ---> %d, others ---> %d" % (number,space,letter,other))
+
+str = input("Enter a string: ")
+Counting(str)
